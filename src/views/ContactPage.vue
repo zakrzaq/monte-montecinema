72a93c10ef46b1348b6d70f71050c5e19191a0a4
Jakub Zakrzewski <j.zakrzewsky@gmail.com>
2022-08-23 10:46:34 +0200

    CONTACT PAGE
    - [x] header
    - [x] details
    - [x] social
    - [x] map
    - [x] responsive

--- src/views/ContactPage.vue
+++ src/views/ContactPage.vue
@@ -1,5 +1,20 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import ContactCard from "@/components/contact/ContactCard.vue";
+</script>
 
-<template>Contact us</template>
+<template>
+  <div class="contact-page">
+    <h1 class="contact-page__header heading-1">Contact Us</h1>
+    <ContactCard />
+  </div>
+</template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.contact-page {
+  margin-top: 64px;
+
+  &__header {
+    margin-bottom: 64px;
+  }
+}
+</style>
