d36b346c1d277390cf4adafe6f48dc452c2c10a6
Jakub Zakrzewski <j.zakrzewsky@gmail.com>
2022-09-01 13:47:53 +0200

    SEANCES: cleanup

--- src/components/seances/DateSelector.vue
+++ src/components/seances/DateSelector.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed, ref, watch } from "vue";
+import { computed } from "vue";
 import { storeToRefs } from "pinia"
 import BaseButton from "../base/BaseButton.vue";
 import BaseSelect from "../base/BaseSelect.vue";
