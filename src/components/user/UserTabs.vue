afc969e7078c20e40885af55f5854ff4010b2bab
Jakub Zakrzewski <j.zakrzewsky@gmail.com>
2022-09-06 16:00:44 +0200

    USER_PAGE:
    - [x] UserTabs

---
+++ src/components/user/UserTabs.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import { computed, ref } from "vue";
+
+const tabs = ["Personal details", "Reservations"];
+const selectedIndex = ref(0);
+const selectedTab = computed(() => tabs[selectedIndex.value]);
+</script>
+
+<template>
+  <div class="user-tabs">
+    <ul class="user-tabs__list">
+      <li
+        v-for="(tab, index) in tabs"
+        :key="index"
+        class="user-tabs__tab"
+        :class="{ 'user-tabs__tab--active': index === selectedIndex }"
+        @click="selectedIndex = index"
+      >
+        {{ tab }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.user-tabs {
+  margin: 64px 0;
+
+  &__list {
+    @include flex-vcenter-hcenter;
+    justify-content: flex-start;
+    gap: 64px;
+  }
+
+  &__tab {
+    @include eczar(normal, 600);
+    font-size: 48px;
+    line-height: 102%;
+    letter-spacing: -0.01em;
+    color: $bombay;
+    cursor: pointer;
+
+    &--active {
+      color: $tuna;
+    }
+  }
+}
+</style>
--- src/views/UserPage.vue
+++ src/views/UserPage.vue
@@ -1,12 +1,15 @@
 <script setup lang="ts">
+import { ref } from "vue";
 import { useUserStore } from "@/stores/user";
+import UserTabs from "@/components/user/UserTabs.vue";
 const userStore = useUserStore();
+const selectedTab = ref("Personal details");
 </script>
 
 <template>
-  <div>
-    <h1>User page</h1>
-    <p>Hello {{ userStore.user?.first_name }}</p>
+  <p>Hello {{ userStore.user?.first_name }}</p>
+  <div class="user-page">
+    <UserTabs v-model="selectedTab" />
   </div>
 </template>
 
